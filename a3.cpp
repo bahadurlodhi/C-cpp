94fd036689c951efb8a4393a1e8c30a3dd247c9a
Bahadur <bahadurlodhi59@gmail.com>
2022-11-20 08:08:51 -0800

    second prgm

---
+++ a3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{  int n;
+  cout<<"Enter any positive number :";
+  cin>>n;
+    for(int i=1;i<=10;i++)
+    {
+        
+                cout<<n<<"*"<<i<<"="<<n*i<<endl;
+    }
+     return 0;
+}
