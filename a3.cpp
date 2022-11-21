5acfeeb61aa773c55cabcc3f0fbf9d49ba06f8c7
Bahadur <bahadurlodhi59@gmail.com>
2022-11-21 08:29:59 -0800

    first commit

--- a3.cpp
+++ a3.cpp
@@ -7,7 +7,6 @@ int main()
   cin>>n;
     for(int i=1;i<=10;i++)
     {
-        
                 cout<<n<<"*"<<i<<"="<<n*i<<endl;
     }
      return 0;
---
+++ a4.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int n,i,j;
+ cout<<"enter the number :";
+ cin>>n;
+ for(i=1;i<=n;i++)
+ {
+  for(j=1;j<=i;j++)
+  
+ {
+    cout<<"* ";
+ 
+ }
+    cout<<"\n";
+ }
+ return 0;
+}
